473fbc3e83326b5d132fd23c86764421daaa9c09
Python121212 <yuu20100530@gmail.com>
2026-04-09 13:01:10 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,16 +1,26 @@
-const CACHE_NAME = 'bm40-android-v1';
-const ASSETS = [
-    './',
-    './index.html',
-    './manifest.json',
-    './js/Main_Controller.js',
-    // ... 他のJSファイル
+const CACHE_NAME = 'bm60-cache-v1';
+const ASSETS_TO_CACHE = [
+  './index.html',
+  './manifest.json',
+  './js/Master_Orchestrator.js',
+  './js/PDF_Raw_Stream.js',
+  // 必要に応じて他のエンジニアJSをここに追加、または動的にキャッシュ
 ];
 
-self.addEventListener('install', (e) => {
-    e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+// インストール時にリソースをキャッシュ
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
 });
 
-self.addEventListener('fetch', (e) => {
-    e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+// オフライン時の応答
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
 });
